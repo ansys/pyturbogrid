cac409ee646651912130f70e0900866af95f887b
Zeljko Lilek <zeljko.lilek@ansys.com>
2023-03-28 16:17:42 -0400

    Initial repository commit based on pyAnsys advanced template

---
+++ src/ansys/turbogrid/core/__init__.py
@@ -0,0 +1,12 @@
+"""
+TurboGrid.
+
+core
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
