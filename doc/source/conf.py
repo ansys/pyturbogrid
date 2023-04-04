ac5bafdf44e150e704129c79c75d3393f66f65d4
zlilek-work <zeljko.lilek@ansys.com>
2023-04-04 13:39:44 -0400

    Update conf.py
    .

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -43,43 +43,43 @@ html_static_path = ["_static"]
 
 ##  =========================== DEFAULT =====
 #
-#"""Sphinx documentation configuration file."""
-#from datetime import datetime#
+# """Sphinx documentation configuration file."""
+# from datetime import datetime#
 #
-#from ansys_sphinx_theme import pyansys_logo_black as logo
+# from ansys_sphinx_theme import pyansys_logo_black as logo
 
 ## Project information
-#project = "ansys-turbogrid"
-#copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
-#author = "ANSYS, Inc."
-#release = version = "0.1.dev0"
+# project = "ansys-turbogrid"
+# copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
+# author = "ANSYS, Inc."
+# release = version = "0.1.dev0"
 
 ## Select desired logo, theme, and declare the html title
-#html_logo = logo
-#html_theme = "ansys_sphinx_theme"
-#html_short_title = html_title = "pyturbogrid"
+# html_logo = logo
+# html_theme = "ansys_sphinx_theme"
+# html_short_title = html_title = "pyturbogrid"
 
 # specify the location of your github repo
-#html_theme_options = {
+# html_theme_options = {
 #    "github_url": "https://github.com/pyansys/pyturbogrid",
 #    "show_prev_next": False,
 #    "show_breadcrumbs": True,
 #    "additional_breadcrumbs": [
 #        ("PyAnsys", "https://docs.pyansys.com/"),
 #    ],
-#}
+# }
 
 ## Sphinx extensions
-#extensions = [
+# extensions = [
 #    "sphinx.ext.autodoc",
 #    "sphinx.ext.autosummary",
 #    "numpydoc",
 #    "sphinx.ext.intersphinx",
 #    "sphinx_copybutton",
-#]
+# ]
 
 ## Intersphinx mapping
-#intersphinx_mapping = {
+# intersphinx_mapping = {
 #    "python": ("https://docs.python.org/dev", None),
 #    # kept here as an example
 #    # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
@@ -88,7 +88,7 @@ html_static_path = ["_static"]
 #    # "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
 #    # "pyvista": ("https://docs.pyvista.org/", None),
 #    # "grpc": ("https://grpc.github.io/grpc/python/", None),
-#}
+# }
 
 ## numpydoc configuration
 ##numpydoc_show_class_members = False
@@ -96,8 +96,8 @@ html_static_path = ["_static"]
 
 ## Consider enabling numpydoc validation. See:
 ## https://numpydoc.readthedocs.io/en/latest/validation.html#
-#numpydoc_validate = True
-#numpydoc_validation_checks = {
+# numpydoc_validate = True
+# numpydoc_validation_checks = {
 #    "GL06",  # Found unknown section
 #    "GL07",  # Sections are in the wrong order.
 #    "GL08",  # The object does not have a docstring
@@ -110,17 +110,17 @@ html_static_path = ["_static"]
 #    # "SS05", # Summary must start with infinitive verb, not third person
 #    "RT02",  # The first line of the Returns section should contain only the
 #    # type, unless multiple values are being returned"
-#}
+# }
 
 
 ## static path
-#html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 ## Add any paths that contain templates here, relative to this directory.
-#templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 ## The suffix(es) of source filenames.
-#source_suffix = ".rst"
+# source_suffix = ".rst"
 
 ## The master toctree document.
-#master_doc = "index"
+# master_doc = "index"
