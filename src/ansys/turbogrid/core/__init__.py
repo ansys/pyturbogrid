fe8d85c3995a6d1ade2cf9dd28cdedce20f8ffa0
zlilek-work <zeljko.lilek@ansys.com>
2023-04-10 16:14:23 -0400

    update
    .

--- src/ansys/turbogrid/core/__init__.py
+++ src/ansys/turbogrid/core/__init__.py
@@ -1,16 +1,3 @@
 __version__ = "0.0.dev0"
 
 # from ansys.turbogrid.core.launcher import launch_turbogrid
-
-# """
-# TurboGrid.
-#
-# core
-# """
-
-# try:
-#     import importlib.metadata as importlib_metadata
-# except ModuleNotFoundError:
-#    import importlib_metadata
-#
-# __version__ = importlib_metadata.version(__name__.replace(".", "-"))
