a612c4c65ca0af5138d0f973dfb8c65dccd75d68
jjoy-work <117776007+jjoy-work@users.noreply.github.com>
2023-05-08 16:48:01 -0400

    Update comments in an example.

--- examples/read_inf_rotor37.py
+++ examples/read_inf_rotor37.py
@@ -4,7 +4,7 @@
 Read Inf Example
 ----------------
 
-This example demonstrates how to launch a PyTurboGrid instance and use it to load a blade model into the associated TurboGrid instance by reading an inf file. Further this will also show how to queery mesh statistics from the session.
+This example demonstrates how to launch a PyTurboGrid instance and use it to load a blade model into the associated TurboGrid instance by reading an inf file. Further this will also show how to query mesh statistics from the session.
 
 """
 import json
@@ -17,7 +17,7 @@ import os.path as ospath
 from ansys.api.turbogrid.launcher import get_turbogrid_exe_path, launch_turbogrid
 
 #################################################################################
-# We want to access the examples folder in the TurboGrid installation available
+# We want to access the examples folder in the TurboGrid installation available.
 # Find the examples folder based on the path to cfxtg found by below call to get_turbogrid_exe_path
 exec_path = get_turbogrid_exe_path()
 turbogrid_install_location = "/".join(exec_path.parts[:-2])
@@ -25,33 +25,33 @@ turbogrid_install_location = turbogrid_install_location.replace("\\", "")
 examples_path_str = turbogrid_install_location + "/examples"
 
 #################################################################################
-# For sanity, ensure the examples folder exists
+# For sanity, ensure the examples folder exists.
 if not ospath.isdir(examples_path_str):
     print("examples folder not found in the TurboGrid installation")
     exit()
 
 #################################################################################
-# Launch turbogrid in the most basic way
-# We need to launch turbogrid from the same path as obtained above while finding the examples location
+# Launch turbogrid in the most basic way.
+# We need to launch turbogrid from the same path as obtained above while finding the examples location.
 turbogrid = launch_turbogrid(turbogrid_path=exec_path)
 
 #################################################################################
-# Read inf for rotor37 example
+# Read inf for rotor37 example.
 turbogrid.read_inf(examples_path_str + "/rotor37/BladeGen.inf")
 
 #################################################################################
-# Generate mesh with the default settings
+# Generate mesh with the default settings.
 turbogrid.unsuspend(object="/TOPOLOGY SET")
 
 #################################################################################
-# Get the mesh statistics from the current session
+# Get the mesh statistics from the current session.
 stats = turbogrid.query_mesh_statistics()
 
 #################################################################################
-# Print out the mesh statistics in a formatting suitable to read
-# json serializer can print nested dictionary content with nice indentation and formatting
+# Print out the mesh statistics in a formatting suitable to read.
+# json serializer can print nested dictionary content with nice indentation and formatting.
 print("Mesh statistics:", json.dumps(stats, indent=2))
 
 #################################################################################
-# quit the turbogrid session
+# Quit the turbogrid session.
 turbogrid.quit()
