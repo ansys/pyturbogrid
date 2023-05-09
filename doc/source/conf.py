21c4cba9d46f8d7cb8f1458961ad3a2877eac851
jjoy-work <117776007+jjoy-work@users.noreply.github.com>
2023-05-09 11:15:26 -0400

    Add src folder to sys path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import os
 import sys
 
-sys.path.append(os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
