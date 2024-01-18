f74e7915be43156d92b11d79132a9a83b801d258
Acohen-work <119608877+Acohen-work@users.noreply.github.com>
2024-01-18 12:35:11 -0500

    formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,56 +1,4 @@
-# # Copyright (c) 2023 ANSYS, Inc. All rights reserved
-# import os
-# import sys
-
-# import pytest
-
-# pyturbogrid_root = os.getenv("PYTURBOGRID_ROOT")
-# if pyturbogrid_root:
-#     sys.path.append(f"{pyturbogrid_root}/src")
-# else:
-#     sys.path.append("./src")
-
-# # Note, internal ANSYS developers will have this available.
-# # External developers will need to have the most up-to-date ansys-turbogrid-api
-# # package installed in order to continue below.
-# pyturbogrid_api_root = os.getenv("PYTURBOGRID_API_ROOT")
-# if pyturbogrid_api_root:
-#     sys.path.append(f"{pyturbogrid_api_root}/src")
-
-
-# from ansys.turbogrid.api import pyturbogrid_core
-
-
-# pytest.socket_port = 5000
-
-
-# # Fixtures are set-up methods. The method is passed as an argument to the test method and will be
-# # run before the method definition.
-# @pytest.fixture
-# def pyturbogrid() -> pyturbogrid_core.PyTurboGrid:
-#     pytest.socket_port += 1
-#     is_debug = os.getenv("PYTURBOGRID_DEBUG")
-#     if is_debug == "1":
-#         additional_args = "-debug"
-#     else:
-#         additional_args = None
-
-#     tg_local_root = os.getenv("PYTURBOGRID_TURBOGRID_LOCAL_ROOT")
-#     if tg_local_root:
-#         additional_kw_args = {"local-root": tg_local_root}
-#     else:
-#         additional_kw_args = None
-
-#     pyturbogrid = launch_turbogrid(
-#         additional_args_str=additional_args,
-#         additional_kw_args=additional_kw_args,
-#         port=pytest.socket_port,
-#     )
-#     print("Success")
-#     return pyturbogrid
-
-
-# Copyright (c) 2023 ANSYS, Inc. All rights reserved
+# Copyright (c) 2024 ANSYS, Inc. All rights reserved
 import ast
 from enum import IntEnum
 import os
@@ -65,7 +13,6 @@ import pytest
 
 from DeployTGContainer import deployed_tg_container, remote_tg_instance
 
-
 pyturbogrid_root = os.getenv("PYTURBOGRID_ROOT")
 if pyturbogrid_root:
     sys.path.append(f"{pyturbogrid_root}/src")
@@ -78,10 +25,9 @@ if pyturbogrid_api_root:
 else:
     raise RuntimeError("PYTURBOGRID_API_ROOT must be defined")
 
-from ansys.turbogrid.core.launcher.launcher import launch_turbogrid
-
-
 from ansys.turbogrid.api import pyturbogrid_core
+
+# from ansys.turbogrid.core.launcher.launcher import launch_turbogrid
 
 
 class TestExecutionMode(IntEnum):
@@ -130,19 +76,22 @@ def pytest_addoption(parser):
     parser.addoption(
         "--ssh_key_filename",
         action="store",
-        help="The location of the ssh key for connection to the TurboGrid container. Used when --execution-mode==CONTAINERIZED",
+        help="The location of the ssh key for connection to the TurboGrid container. "
+        "Used when --execution-mode==CONTAINERIZED",
     )
     parser.addoption(
         "--container_env_dict",
         action="store",
         default="{}",
-        help="A dictionary for additional environment variables to pass to the container. Used when --execution-mode==CONTAINERIZED",
+        help="A dictionary for additional environment variables to pass to the container. "
+        "Used when --execution-mode==CONTAINERIZED",
     )
     parser.addoption(
         "--keep_stopped_containers",
         action="store_true",
         default=False,
-        help="Whether or not to leave the stopped containers around without removing them. Used when --execution-mode==CONTAINERIZED",
+        help="Whether or not to leave the stopped containers around without removing them. "
+        "Used when --execution-mode==CONTAINERIZED",
     )
     parser.addoption(
         "--remote_command",
@@ -222,7 +171,8 @@ def get_enum_value_from_env(
         enum_value = enum_type[enum_value]
     except KeyError:
         raise RuntimeError(
-            f"Environment variable {env_var_name} was set to an invalid value '{enum_value}'. Valid values include {valid_values_str}."
+            f"Environment variable {env_var_name} was set to an invalid value '{enum_value}'. "
+            f"Valid values include {valid_values_str}."
         )
     return enum_value
 
@@ -236,7 +186,8 @@ def get_tg_container(pytestconfig, socket_port: int) -> deployed_tg_container:
     # The path to cfxtg_command is standardized by the container, so just replace the command name.
     # This allows image developers to write custom cfxtg commands.
     cfxtg_command: str = pytestconfig.getoption("cfxtg_command_name")
-    cfxtg_command = f"./v{pytestconfig.getoption('cfx_version')}/TurboGrid/bin/{cfxtg_command} -py -control-port {socket_port}"
+    cfxtg_command = f"./v{pytestconfig.getoption('cfx_version')}/TurboGrid/bin/{cfxtg_command} "
+    f"-py -control-port {socket_port}"
 
     license_server: str = pytestconfig.getoption("license_file")
     pytest.ftp_port = get_open_port()
