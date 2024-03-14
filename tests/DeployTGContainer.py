1a84f6e8defa59e9ac08d557f863fc2f1b550626
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-03-14 10:45:06 +0100

    feat: add precommt for licece

--- tests/DeployTGContainer.py
+++ tests/DeployTGContainer.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import platform
 import subprocess
 import time
@@ -121,9 +143,11 @@ class remote_tg_instance:
         print(f"       is_linux = {self.is_linux}")
         print("\n")
         subprocess.run(
-            "env"
-            if self.is_linux
-            else '"C:/Program Files/PowerShell/7/pwsh.exe" -Command gci env:',
+            (
+                "env"
+                if self.is_linux
+                else '"C:/Program Files/PowerShell/7/pwsh.exe" -Command gci env:'
+            ),
             shell=True,
         )
         print("######### Spin up remote process #########")
