5d35fb8447b8667da72d3c62c8bbc6f21efb7518
Acohen-work <119608877+Acohen-work@users.noreply.github.com>
2024-05-29 16:47:55 -0400

    3.9 fix

--- src/ansys/turbogrid/core/launcher/container_helpers.py
+++ src/ansys/turbogrid/core/launcher/container_helpers.py
@@ -26,7 +26,6 @@ import time
 from fabric import Connection
 
 
-@staticmethod
 def get_open_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
