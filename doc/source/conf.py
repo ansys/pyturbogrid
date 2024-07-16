9589ff26cf8a5cc1501ee243f6fb0366f3022f81
Acohen-work <119608877+Acohen-work@users.noreply.github.com>
2024-07-16 10:03:20 -0400

    try another filter

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,19 +1,18 @@
 # Copyright (c) 2023 ANSYS, Inc. All rights reserved
 from datetime import datetime
+import logging
 import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
-import sphinx.util.logging
-from sphinx.util.logging import SphinxWarningLogRecord
 from sphinx_gallery.sorting import FileNameSortKey
 
 from ansys.turbogrid.core import __version__
 
 
-class MyFilter(sphinx.util.logging.WarningFilter):
-    def filter(self, record: SphinxWarningLogRecord) -> bool:
+class InlineTextWarningFilter(logging.Filter):
+    def filter(self, record):
         if (
             "Inline interpreted text or phrase reference start-string without end-string."
             in record.msg
@@ -22,7 +21,7 @@ class MyFilter(sphinx.util.logging.WarningFilter):
         return True
 
 
-sphinx.util.logging.getLogger("sphinx").addFilter(MyFilter())
+logging.getLogger("sphinx").addFilter(InlineTextWarningFilter())
 
 # Configuration file for the Sphinx documentation builder.
 #
